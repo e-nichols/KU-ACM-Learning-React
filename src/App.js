b11b3f5c04a9b095b52f1d5d206d3ad6b48af5a3
Evan Nichols <evannichols@MacBook-Pro-28.local>
2017-02-07 00:20:53 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+//Our Components
+import HelloWorld from './components/HelloWorld'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>KU ACM: Learning React</h2>
+        </div>
+        <p className="App-intro">
+          Example Components Below.
+          <HelloWorld name="Evan" />
+          <HelloWorld name="Charles" />
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
